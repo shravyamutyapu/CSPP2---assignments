3076c0a4f65429c9ec1a90f9fb306323501d426f
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-08-28 17:56:30 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- M2/Assignment-3/Solution.java
+++ M2/Assignment-3/Solution.java
@@ -9,6 +9,7 @@ public class Solution {
     /**
     Do not modify this main function.
     **/
+    private Solution() { } 
     public static void main(String[] args) {
         /*main
         method
