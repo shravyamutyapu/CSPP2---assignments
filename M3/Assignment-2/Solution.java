579859aa44229a3d1b4b43aeb59c66920b02fde5
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-08-29 19:26:21 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- M3/Assignment-2/Solution.java
+++ M3/Assignment-2/Solution.java
@@ -1,24 +1,37 @@
-
+/**
+    *importing scanner.
+    */
 import java.util.Scanner;
-/*
-	Do not modify this main function.
-	*/
-public class Solution {
-/* Fill the main function to print the number of 7's between 1 to n*/
-    public static void main(String[] args) {
+/**
+    *Do not modify this main function.
+    */
+public final class Solution {
+/**
+    *default constructor.
+    */
+    private Solution() {
 
-        Scanner s=new Scanner(System.in);      
-        int n = s.nextInt();
-        int count=0;
-        for(int i=1;i<n;i++){
-        	if(i/7==0)
-        	{
-        		count=count+1;
-        	}
-        System.out.println(count);	
-        
-       
-        
     }
+/**
+*Fill the main function to print the number of 7's between 1 to n.
+*@param args String
+*/
+    public static void main(final String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        final int n = s.nextInt();
+        int count = 0;
+        final int a = 7, b = 10;
+        for (int i = 1; i <= n; i++) {
+            int temp = i;
+            while (temp != 0) {
+                int j = temp % b;
+                if (j == a) {
+                    count++;
+                }
+                temp = temp / b;
+            }
         }
+        System.out.println(count);
+    }
 }
