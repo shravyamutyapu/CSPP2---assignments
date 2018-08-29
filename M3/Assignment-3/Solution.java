79782059bb4e08c6c8eeeafc99b668ccc04648a6
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-08-29 18:10:50 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- M3/Assignment-3/Solution.java
+++ M3/Assignment-3/Solution.java
@@ -16,7 +16,7 @@ public class Solution {
      * @param args [description]
      **/
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
         Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
