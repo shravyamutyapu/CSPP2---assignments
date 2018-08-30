072052aa04a8728654930ca85afb6d34c810e120
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-08-30 14:20:49 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- M4/Assignment-1/Solution.java
+++ M4/Assignment-1/Solution.java
@@ -1,27 +1,27 @@
 import java.util.*;
-public class Solution
-{/*
-	Fill this main function to print maximum of given array
-	*/
-	public static void main(String[] args)
-	{   
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		int arr[];
-
-		arr = new int[n];
-		for(int i=0;i<n;i++){
-
-     	arr[i]= sc.nextInt();
+public class Solution {
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * 
+ * @param args [description]
+ **/
+    
+    public static void main(final String[] args) {   
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[];
+        arr = new int[n];
+        for(int i=0;i<n;i++){
+        arr[i] = sc.nextInt();
         }
         int temp = arr[0];
         for(int i=0;i<n;i++){
-        	if(arr[i]>temp){
-        		temp = arr[i];
-        	}
-        
+            if(arr[i]>temp){
+                temp = arr[i];
+            }
         }
         System.out.println(temp);
 
-	}
+    }
 }
