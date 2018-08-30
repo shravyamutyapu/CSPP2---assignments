cc920c4215fa51bc1686c9c0287444017d35f906
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-08-30 14:15:02 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- M4/Assignment-1/Solution.java
+++ M4/Assignment-1/Solution.java
@@ -12,7 +12,7 @@ public class Solution
 		arr = new int[n];
 		for(int i=0;i<n;i++){
 
-     	arr[i]=sc.nextInt();
+     	arr[i]=new Scanner(System.in).nextInt();
         }
         int temp = arr[0];
         for(int i=0;i<n;i++){
