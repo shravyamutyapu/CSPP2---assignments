bbbfcbcfdae2c7e4503a5c4246a6d64301a0bf35
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-08-30 14:27:45 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- M4/Assignment-1/Solution.java
+++ M4/Assignment-1/Solution.java
@@ -3,20 +3,18 @@ public class Solution {
 /**
  * @brief [brief description]
  * @details [long description]
- * 
  * @param args [description]
  **/
-    
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int arr[];
         arr = new int[n];
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
         arr[i] = sc.nextInt();
         }
         int temp = arr[0];
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             if (arr[i] > temp) {
                 temp = arr[i];
             }
