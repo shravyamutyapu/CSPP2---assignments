cfa1f2341475c99e327f2569be74483cbabe6176
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-09-02 18:39:45 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

--- M6/Assignment-2/Solution.java
+++ M6/Assignment-2/Solution.java
@@ -23,47 +23,52 @@ final class Solution {
      * @return     Matrix of the rounded elements
      */
     static int[][] rndHun(final int[][] a, final int rows, final int columns) {
+     for (int i = 0; i < a[0].length; i++) {
+            for (int j = 0; j < a.length; j++){
+                a[i][j] = ((a[i][j]%100)*100);
 
-    // write ypur code here
-        final int b = 50;
-        final int c = 100;
-        final int d = 150;
-        final int e = 200;
-        final int f = 250;
-        final int g = 300;
-        final int h = 350;
-        final int l = 400;
-        final int m = 450;
-        final int p = 500;
-        final int q = 550;
+     }
+ }
+    // // write ypur code here
+    //     final int b = 50;
+    //     final int c = 100;
+    //     final int d = 150;
+    //     final int e = 200;
+    //     final int f = 250;
+    //     final int g = 300;
+    //     final int h = 350;
+    //     final int l = 400;
+    //     final int m = 450;
+    //     final int p = 500;
+    //     final int q = 550;
 
-        for (int i = 0; i < a[0].length; i++) {
-            for (int j = 0; j < a.length; j++) {
-                if (a[i][j] < b) {
-                    a[i][j] = 0;
-                } else if (a[i][j] >= b && a[i][j] <= c) {
-                    a[i][j] = c;
-                } else if (a[i][j] > c && a[i][j] <= d) {
-                    a[i][j] = c;
-                } else if (a[i][j] > d && a[i][j] <= e) {
-                    a[i][j] = e;
-                } else if (a[i][j] > e && a[i][j] <= f) {
-                    a[i][j] = e;
-                } else if (a[i][j] > f && a[i][j] <= g) {
-                    a[i][j] = g;
-                } else if (a[i][j] > g && a[i][j] <= h) {
-                    a[i][j] = g;
-                } else if (a[i][j] > h && a[i][j] <= l) {
-                    a[i][j] = l;
-                } else if (a[i][j] > l && a[i][j] <= m) {
-                    a[i][j] = l;
-                } else if (a[i][j] > m && a[i][j] <= p) {
-                    a[i][j] = p;
-                } else if (a[i][j] > p && a[i][j] <= q) {
-                    a[i][j] = p;
-                }
-            }
-        }
+    //     for (int i = 0; i < a[0].length; i++) {
+    //         for (int j = 0; j < a.length; j++) {
+    //             if (a[i][j] < b) {
+    //                 a[i][j] = 0;
+    //             } else if (a[i][j] >= b && a[i][j] <= c) {
+    //                 a[i][j] = c;
+    //             } else if (a[i][j] > c && a[i][j] <= d) {
+    //                 a[i][j] = c;
+    //             } else if (a[i][j] > d && a[i][j] <= e) {
+    //                 a[i][j] = e;
+    //             } else if (a[i][j] > e && a[i][j] <= f) {
+    //                 a[i][j] = e;
+    //             } else if (a[i][j] > f && a[i][j] <= g) {
+    //                 a[i][j] = g;
+    //             } else if (a[i][j] > g && a[i][j] <= h) {
+    //                 a[i][j] = g;
+    //             } else if (a[i][j] > h && a[i][j] <= l) {
+    //                 a[i][j] = l;
+    //             } else if (a[i][j] > l && a[i][j] <= m) {
+    //                 a[i][j] = l;
+    //             } else if (a[i][j] > m && a[i][j] <= p) {
+    //                 a[i][j] = p;
+    //             } else if (a[i][j] > p && a[i][j] <= q) {
+    //                 a[i][j] = p;
+    //             }
+    //         }
+    //     }
         return a;
     }
     /**
