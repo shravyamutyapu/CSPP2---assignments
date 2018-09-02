05de9ec9e8434de699f06809c3a0e24b4c3c2b7d
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-09-02 18:40:53 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

--- M6/Assignment-2/Solution.java
+++ M6/Assignment-2/Solution.java
@@ -25,7 +25,7 @@ final class Solution {
     static int[][] rndHun(final int[][] a, final int rows, final int columns) {
      for (int i = 0; i < a[0].length; i++) {
             for (int j = 0; j < a.length; j++){
-                a[i][j] = ((a[i][j]%100)*100);
+                a[i][j] = ((a[i][j]/100)*100);
 
      }
  }
