885cc329a36a56928ae11f39a79270b04b1ec45e
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-09-03 21:00:46 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- M7/Assignment-1/Solution.java
+++ M7/Assignment-1/Solution.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 	final class InputValidator {
 		private String str;
 	    private int len;
@@ -9,29 +8,20 @@ import java.util.Scanner;
 	    }
 	    public boolean validateData() {
 	final int b = 6;
-	if(len>=b){
+	if(len >= b){
 		return true;
 	}
 	return false;
 }
-       
-
 }
-
-
-
-
 final class Solution
 {
 	private Solution() { }
-	public static void main(String args[])
-    {
-    	Scanner s=new Scanner(System.in);
-    	String input=s.next();
-    	InputValidator i=new InputValidator(input);    	
+	public static void main(String args[]) {
+    	Scanner s = new Scanner(System.in);
+    	String input = s.next();
+    	InputValidator i = new InputValidator(input);    	
     	System.out.println(i.validateData());
-
     }
-
 }
 
