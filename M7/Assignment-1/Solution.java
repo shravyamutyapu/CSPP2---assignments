c29be7e3de0312e037f57496bcfc8e468addd33e
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-09-03 21:05:44 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- M7/Assignment-1/Solution.java
+++ M7/Assignment-1/Solution.java
@@ -1,33 +1,32 @@
 import java.util.Scanner;
-	final class InputValidator {
-		/** 
-		   Declaring a string 
-		**/
-		private String str;
-		/** 
-		   Declaring string length 
-		**/
-	    private int len;
-	    InputValidator(final String str1) {
-	    str = str1;
-        len = str.length();	
-	    }
-	    public boolean validateData() {
-	final int b = 6;
-	if(len >= b){
-		return true;
-	}
-	return false;
+    final class InputValidator {
+        /** 
+           Declaring a string 
+        **/
+        private String str;
+        /** 
+           Declaring string length 
+        **/
+        private int len;
+        InputValidator(final String str1) {
+        str = str1;
+        len = str.length(); 
+        }
+        public boolean validateData() {
+    final int b = 6;
+    if(len >= b){
+        return true;
+    }
+    return false;
 }
 }
-final class Solution
-{
-	private Solution() { }
-	public static void main(String args[]) {
-    	Scanner s = new Scanner(System.in);
-    	String input = s.next();
-    	InputValidator i = new InputValidator(input);    	
-    	System.out.println(i.validateData());
+final class Solution {
+    private Solution() { }
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);       
+        System.out.println(i.validateData());
     }
 }
 
