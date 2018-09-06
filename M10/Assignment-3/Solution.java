2cc9847877f0b3ef817ec359695622b922789f54
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-09-06 23:37:54 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- M10/Assignment-3/Solution.java
+++ M10/Assignment-3/Solution.java
@@ -36,9 +36,7 @@ class Student {
 }
 
 public class Solution {
-    
-
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
         String objectType = stdin.nextLine();
@@ -57,7 +55,7 @@ public class Solution {
                             listString.add(tokens[1]);
                         break;
                         case "addAll":
-                        if(tokens.length==2){
+                        if(tokens.length == 2){
                         String[] t1 = tokens[1].split(",");
                         listString.addAll(t1);
                         }
@@ -104,7 +102,7 @@ public class Solution {
                             listInteger.add(Integer.parseInt(tokens[1]));
                         break;
                         case "addAll":
-                        if(tokens.length==2){
+                        if(tokens.length == 2){
                         String[] t1 = tokens[1].split(",");
                         Integer[] temp = new Integer[t1.length];
                         for (int i = 0;i < temp.length; i++ ) {
@@ -155,7 +153,7 @@ public class Solution {
                             listFloat.add(Float.parseFloat(tokens[1]));
                         break;
                         case "addAll":
-                        if(tokens.length==2){
+                        if(tokens.length == 2){
                         String[] t1 = tokens[1].split(",");
                         Float temp[] = new Float[t1.length];
                         for (int i = 0; i < t1.length ;i++ ) {
@@ -206,10 +204,10 @@ public class Solution {
                             listCharacter.add(tokens[1].charAt(0));
                         break;
                         case "addAll":
-                        if(tokens.length==2){
+                        if(tokens.length ==  2){
                         String[] t1 = tokens[1].split(",");
                         Character[] temp = new Character[t1.length];
-                        for (int i = 0; i < t1.length ;i++ ) {
+                        for (int i = 0; i < t1.length ;i++) {
                             temp[i] = t1[i].charAt(0);
                         }
                         listCharacter.addAll(temp);
@@ -257,7 +255,7 @@ public class Solution {
                             listDouble.add(Double.parseDouble(tokens[1]));
                         break;
                         case "addAll":
-                        if(tokens.length==2){
+                        if(tokens.length == 2){
                         String[] t1 = tokens[1].split(",");
                         Double[] temp = new Double[t1.length];
                         for (int i = 0; i < t1.length ;i++ ) {
@@ -308,7 +306,7 @@ public class Solution {
                             listStudent.add(new Student(tokens[1]));
                         break;
                         case "addAll":
-                        if(tokens.length==2){
+                        if(tokens.length == 2){
                         String[] t1 = tokens[1].split(",");
                         Student[] temp = new Student[t1.length];
                         for (int i = 0; i < t1.length ;i++ ) {
