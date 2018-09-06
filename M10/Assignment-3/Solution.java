c6f05d8dc58af70610e3de1bf76ba2315150bb42
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-09-06 23:43:49 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- M10/Assignment-3/Solution.java
+++ M10/Assignment-3/Solution.java
@@ -8,20 +8,22 @@ class Student {
      private variable.
      **/
     private String name;
-    /**
-    constructor
-     **/
-    
-    public Student(String name) {//A constructor used to initialize the instance variables
+   /** 
+    * @param name [description]
+    * @return [description]
+    */
+    public Student(final String name) { //A constructor used to initialize the instance variables
         this.name = name;
     }
-
-    public String getName() {//Getter method which returns the value of instance variable
+    /**
+     get name.
+     **/
+    public String getName() { //Getter method which returns the value of instance variable
         return this.name;
     }
 
     @Override
-    public boolean equals(Object other) { //This method is to check if two students names are equal or not
+    public boolean equals(Object other) { //This method is to check if  two students names are equal or not
         if (!(other instanceof Student)) {
             return false;
         }
@@ -55,7 +57,7 @@ public class Solution {
                             listString.add(tokens[1]);
                         break;
                         case "addAll":
-                        if(tokens.length == 2){
+                        if (tokens.length == 2){
                         String[] t1 = tokens[1].split(",");
                         listString.addAll(t1);
                         }
@@ -102,7 +104,7 @@ public class Solution {
                             listInteger.add(Integer.parseInt(tokens[1]));
                         break;
                         case "addAll":
-                        if(tokens.length == 2){
+                        if (tokens.length == 2){
                         String[] t1 = tokens[1].split(",");
                         Integer[] temp = new Integer[t1.length];
                         for (int i = 0;i < temp.length; i++ ) {
@@ -153,7 +155,7 @@ public class Solution {
                             listFloat.add(Float.parseFloat(tokens[1]));
                         break;
                         case "addAll":
-                        if(tokens.length == 2){
+                        if (tokens.length == 2){
                         String[] t1 = tokens[1].split(",");
                         Float temp[] = new Float[t1.length];
                         for (int i = 0; i < t1.length ;i++ ) {
@@ -204,7 +206,7 @@ public class Solution {
                             listCharacter.add(tokens[1].charAt(0));
                         break;
                         case "addAll":
-                        if(tokens.length ==  2){
+                        if (tokens.length ==  2){
                         String[] t1 = tokens[1].split(",");
                         Character[] temp = new Character[t1.length];
                         for (int i = 0; i < t1.length ;i++) {
@@ -255,7 +257,7 @@ public class Solution {
                             listDouble.add(Double.parseDouble(tokens[1]));
                         break;
                         case "addAll":
-                        if(tokens.length == 2){
+                        if (tokens.length == 2){
                         String[] t1 = tokens[1].split(",");
                         Double[] temp = new Double[t1.length];
                         for (int i = 0; i < t1.length ;i++ ) {
@@ -306,7 +308,7 @@ public class Solution {
                             listStudent.add(new Student(tokens[1]));
                         break;
                         case "addAll":
-                        if(tokens.length == 2){
+                        if (tokens.length == 2){
                         String[] t1 = tokens[1].split(",");
                         Student[] temp = new Student[t1.length];
                         for (int i = 0; i < t1.length ;i++ ) {
