a59551a6793d96411b7abc89028865184733744e
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-09-14 17:39:07 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

---
+++ M16/bookyourshow/Item.java
@@ -0,0 +1,11 @@
+class Item {
+	String productName;
+	int quantity;
+	double price;
+	Item(String productname, int quan, double unitprice) {
+		this.productName = productname;
+		this.quantity = quan;
+		this.price = unitprice;
+	}
+
+}
