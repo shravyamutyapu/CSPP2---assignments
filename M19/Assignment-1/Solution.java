c240bb41fd628e57c2124420857aa72cb45e378f
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-09-15 12:02:10 +0530

    "check_style_score -> 0 of 13 passed. style score: 0/1 "

--- M19/Assignment-1/Solution.java
+++ M19/Assignment-1/Solution.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
  * Solution class for code-eval.
  */
 public final class Solution {
-     /**
-     * Constructs the object.
-     */
-    static class Quiz{
+    /**
+    * Constructs the object.
+    */
+    static class Quiz {
         int size;
-        Quiz(){
+        Quiz() {
             int size = 0;
         }
 
-     }
-    private Solution() {
+
+    private void Solution() {
         // leave this blank
     }
     /**
@@ -23,37 +23,37 @@ public final class Solution {
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
-         // instantiate this Quiz
+        // instantiate this Quiz
         Quiz q = new Quiz();
-         // code to read the test cases input file
+        // code to read the test cases input file
         Scanner s = new Scanner(System.in);
         // check if there is one more line to process
         while (s.hasNext()) {
             // read the line
             String line = s.nextLine();
-             // split the line using space
+            // split the line using space
             String[] tokens = line.split(" ");
-              // based on the list operation invoke the corresponding method
+            // based on the list operation invoke the corresponding method
             switch (tokens[0]) {
-                case "LOAD_QUESTIONS":
+            case "LOAD_QUESTIONS":
                 System.out.println("|----------------|");
                 System.out.println("| Load Questions |");
                 System.out.println("|----------------|");
                 loadQuestions(s, q, Integer.parseInt(tokens[1]));
                 break;
-                case "START_QUIZ":
+            case "START_QUIZ":
                 System.out.println("|------------|");
                 System.out.println("| Start Quiz |");
                 System.out.println("|------------|");
                 startQuiz(s, q, Integer.parseInt(tokens[1]));
                 break;
-                case "SCORE_REPORT":
+            case "SCORE_REPORT":
                 System.out.println("|--------------|");
                 System.out.println("| Score Report |");
                 System.out.println("|--------------|");
                 displayScore(q);
                 break;
-                default:
+            default:
                 break;
             }
         }
@@ -69,23 +69,25 @@ public final class Solution {
         // write your code here to read the questions from the console
         // tokenize the question line and create the question object
         // add the question objects to the quiz class
-        //Quiz quiz = new Quiz[questionCount];
-        Scanner s1 = new Scanner(System.in);
-        Quiz s2 = new Quiz();
+        Quiz quiz1 = new Quiz();
+        Scanner sc = new Scanner(System.in);
         //Object obj = new Object[10];
-        String[] lines = s.nextLine().split(":");
-        // lines = lines.;
-        String[] choices = lines[1].split(",");
-        String qn = lines[0];
-        int correct = Integer.parseInt(lines[2]);
-        int maxmarks = Integer.parseInt(lines[3]);
-        int penalty = Integer.parseInt(lines[4]);
-        for(int q = 0; q<questionCount;q++){
-        for(int i = 1;i<=choices.length;i++){
-             System.out.print("choice "+i+"   ");
-       }
-       System.out.println("/n");
-   }
+        int qncount = Integer.parseInt(sc.nextLine());
+        System.out.println(qncount +" are added to the quiz");
+        // for (int i = 1; i <= qncount; i++) {
+        //     String[] lines = sc.nextLine().split(":");
+        //     // lines = lines.;
+        //     String[] choices = lines[1].split(",");
+        //     String qn = lines[0];
+        //     int correct = Integer.parseInt(lines[2]);
+        //     int maxmarks = Integer.parseInt(lines[3]);
+        //     int penalty = Integer.parseInt(lines[4]);
+
+
+        }
+
+
+
 
     }
 
@@ -100,6 +102,13 @@ public final class Solution {
         // write your code here to display the quiz questions
         // read the user responses from the console
         // store the user respones in the quiz object
+
+        // for (int q = 0; q < s.questionCount; q++) {
+        //     for (int i = 1; i <= choices.length; i++) {
+        //         System.out.print("choice " + i + "   ");
+        //     }
+        //     System.out.println("");
+        // }
 
 
 
@@ -116,7 +125,7 @@ public final class Solution {
         // write your code here to display the score report
         int totalScore;
         //for(int i = 0;i<questionCount;i++){
-            //totalScore += score[i];
-        }
+        //totalScore += score[i];
     }
+}
 //}
